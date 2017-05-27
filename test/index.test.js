c23bb5d98616a3e8ccab65cb513e65a390ed4edc
François de Metz <francois@2metz.fr>
2017-05-27 23:44:47 +0200

    Don't expose require and version.

--- test/index.test.js
+++ test/index.test.js
@@ -4,7 +4,7 @@ var analytics = require('../lib');
 var assert = require('proclaim');
 
 describe('analytics', function() {
-  it('should expose a .VERSION', function() {
+  xit('should expose a .VERSION', function() {
     var pkg = require('../package.json');
     assert.equal(analytics.VERSION, pkg.version);
   });
